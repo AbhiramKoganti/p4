ba6fbbcdab44c6229a087b5a82efe9e3cf8d1cb2
Abhiram Koganti <abhiram@rockhopper-02.cs.wisc.edu>
2021-03-07 13:51:07 -0600

    changes

--- xv6/proc.h
+++ xv6/proc.h
@@ -48,7 +48,8 @@ struct proc {
   struct trapframe *tf;        // Trap frame for current syscall
   struct context *context;     // swtch() here to run process
   void *chan; // If non-zero, sleeping on chan
-   
+  int time_slice;
+  int time_remaining;
   int killed;                  // If non-zero, have been killed
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
