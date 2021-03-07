00a906a29a101846abbe0de77c3ec0bd5d9a635b
William Mulvaney <mulvaney@royal-28.cs.wisc.edu>
2021-03-07 12:29:58 -0600

    Added valid bit to proc struct so we can check if return of dequeue is valid

--- xv6/proc.h
+++ xv6/proc.h
@@ -32,6 +32,9 @@ struct context {
   uint eip;
 };
 
+
+
+
 enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
@@ -44,13 +47,24 @@ struct proc {
   struct proc *parent;         // Parent process
   struct trapframe *tf;        // Trap frame for current syscall
   struct context *context;     // swtch() here to run process
-  void *chan;                  // If non-zero, sleeping on chan
-  
+  void *chan; // If non-zero, sleeping on chan
+   
+  int valid; 		       // TODO: review this added field
   int killed;                  // If non-zero, have been killed
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
 };
+
+// Struct to hold the process queue
+// Implemented with an array of fixed length
+struct proc_queue{
+  int head;
+  int tail;   
+  int size;
+  struct proc proc[NPROC];
+};
+
 
 // Process memory is laid out contiguously, low addresses first:
 //   text
