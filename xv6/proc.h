1df05e5a26d0add8f5abf989c4b5fc3fc52f2c12
William Mulvaney <mulvaney@royal-01.cs.wisc.edu>
2021-03-07 13:02:47 -0600

    no more valid bit

--- xv6/proc.h
+++ xv6/proc.h
@@ -49,7 +49,6 @@ struct proc {
   struct context *context;     // swtch() here to run process
   void *chan; // If non-zero, sleeping on chan
    
-  int valid; 		       // TODO: review this added field
   int killed;                  // If non-zero, have been killed
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
